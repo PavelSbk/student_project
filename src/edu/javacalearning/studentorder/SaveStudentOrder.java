069abb21fb6a8c2c6cf630169b901f0d3e8e9d55
Pavel Shebeko <shebeko_pavel@mail.ru>
2023-01-30 15:51:32 +0300

    refactoring

--- src/edu/javacalearning/studentorder/SaveStudentOrder.java
+++ src/edu/javacalearning/studentorder/SaveStudentOrder.java
@@ -1,8 +1,14 @@
 package edu.javacalearning.studentorder;
 
+import edu.javacalearning.studentorder.domain.StudentOrder;
+
 public class SaveStudentOrder {
 
     public static void main(String[] args) {
-
+        StudentOrder so = new StudentOrder();
+        so.sethFirstName("Alex");
+        so.sethLastName("Petrov");
+        so.setwFirstName("Mari");
+        so.setwLastName("Petrova");
     }
 }
