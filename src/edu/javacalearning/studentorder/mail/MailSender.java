4e77b344845a21b5754c68b53913018c550b9060
Pavel Shebeko <shebeko_pavel@mail.ru>
2023-01-31 17:18:21 +0300

    refactoring

--- src/edu/javacalearning/studentorder/mail/MailSender.java
+++ src/edu/javacalearning/studentorder/mail/MailSender.java
@@ -4,7 +4,7 @@ import edu.javacalearning.studentorder.domain.StudentOrder;
 
 public class MailSender {
 
-    public void sendMail(StudentOrder so){
+    public void sendMail(StudentOrder so) {
         System.out.println("Mail sending");
     }
 }
